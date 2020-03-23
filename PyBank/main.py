31887a642d171814616e9c8685b2659c30c016d6
LorenzaC <lorenza.culotta@northwestern.edu>
2020-03-23 14:49:09 -0500

    uploaded completed main.py and txt

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,54 +1,70 @@
-#NOTE: In addition, your final script should both print the analysis to the terminal 
-#and export a text file with the results.
-
 import csv
 
 budget_origin=('budget_data.csv')
 
 #lists for financial analysis (NEEDED IN VERSION WITHOUT COMPREHENSION)
 total_months = []       #total number of months included in the dataset   NOT NEEDED
-net_total = 0          #net total amount of "Profit/Losses" over the entire period
-list_profit_losses = []
-#NOT NEEDED average_change = []     #average of the changes in "Profit/Losses" over the entire period
-#NOT NEEDED greatest_increase = []  #greatest increase in profits (date and amount) over the entire period
-#NOT NEEDED greatest_decrease = []  #greatest decrease in losses (date and amount) over the entire period
+total_net = 0           #net total amount of "Profit/Losses" over the entire period
+list_profits = []       #list of profit/losses values
+list_changes = []       #list changes in "Profit/Losses" over the entire period
 
-
-
-
-with open(budget_origin, 'r') as csvfile:
+with open(budget_origin, 'r', newline='\n') as csvfile:
     csvreader=csv.reader(csvfile, delimiter=',')
 
     #find header
     csv_header=next(csvreader)
-    #print(f'CSV Header: {csv_header}')
     
+    #populate list of months and list of profits/losses
     for row in csvreader:
         date=row[0]
         total_months.append(date)
       
-        profit_losses = row[1]
-        list_profit_losses.append(int(profit_losses))
-    
-        net_total += int(profit_losses)
+        profits = row[1]
+        list_profits.append(int(profits))
         
+        #calculate net total
+        total_net += int(profits)
+
+    #calculate changes from one month to another and populate list of changes   
+    for profits in list_profits:
+        idx= list_profits.index(profits)
+        change = int(list_profits[idx]) - int(list_profits[idx-1])
+        list_changes.append(change)
+        
+  
     #calculate average change
-    avg_change= round(net_total / len(total_months),2)
-    
-    #find greatest increase
-    max_profit = max(list_profit_losses)
-    max_index = list_profit_losses.index(max(list_profit_losses))
-    
-    #find greatest decrease
-    min_profit = min(list_profit_losses)
-    min_index = list_profit_losses.index(min(list_profit_losses))
-    
+    list_changes.pop(0)
+    avg_change= round(sum(list_changes) / len(list_changes),2)
 
+    #calculate greatest increase
+    max_profit = max(list_changes)
+    max_index = list_changes.index(max(list_changes)) +1
 
-       
+    
+    #calculate greatest decrease
+    min_profit = min(list_changes)
+    min_index = list_changes.index(min(list_changes)) +1
+    
+    #PRINT RESULTS ON TERMINAL
+    print("-----------------------------------")
+    print("Financial Analysis")
+    print("-----------------------------------")
     print(f'Total Months: {int(len(total_months))}')
-    print(f'Total: ${int(net_total)}')
+    print(f'Total: ${int(total_net)}')
     print(f'Average Change: {avg_change}')
     print(f'Greatest Increase: {total_months[max_index]} (${max_profit})')
     print(f'Greatest Decrease: {total_months[min_index]} (${min_profit})')
+
+#CREATING OUTPUT FILE
+#output file path
+analysis=('Financial_Analysis.txt')
+
+with open(analysis, 'w', newline='\n') as text:
+    text.write("Financial Analysis\n")
+    text.write("------------------\n")
+    text.write(f'Total Months: {int(len(total_months))}\n')
+    text.write(f'Total: ${int(total_net)}\n')
+    text.write(f'Average Change: {avg_change}\n')
+    text.write(f'Greatest Increase: {total_months[max_index]} (${max_profit})\n')
+    text.write(f'Greatest Decrease: {total_months[min_index]} (${min_profit})')
 
